8dbda9644f8e4f3cc64625ff1f7a8f9d1b5db17c
michael <1022986@student.sae.edu.au>
2021-06-17 14:54:56 +0800

    mouse rotation script

--- Assets/Scripts/MouseFollow.cs
+++ Assets/Scripts/MouseFollow.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class MouseFollow : MonoBehaviour
 {
+
+    public Vector3 objectPos;
+    public Vector3 mousePos;
+    public float angle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,14 @@ public class MouseFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        objectPos = this.transform.position;
+
+
+        this.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mousePos.y - objectPos.y, mousePos.x - objectPos.x)* Mathf.Rad2Deg);
+
+
+
     }
 }
