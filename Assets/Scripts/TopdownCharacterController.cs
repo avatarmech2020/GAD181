9ed431027f081b7188233b209ad69c416c4b2ad6
Caitie <caitlyn.s.lichnovsky@outlook.com>
2021-07-22 15:38:20 +0800

    trying to get animations to work

--- Assets/Scripts/TopdownCharacterController.cs
+++ Assets/Scripts/TopdownCharacterController.cs
@@ -12,8 +12,13 @@ public class TopdownCharacterController : MonoBehaviour
     public Camera mainCamera;
     public Vector3 offset;
     public Animator Animator;
-
-
+    public bool moveOn = false;
+    private void Start()
+    {
+        Animator = GetComponent<Animator>();
+      
+            
+    }
     // Update is called once per frame
     void Update()
     {
@@ -23,22 +28,23 @@ public class TopdownCharacterController : MonoBehaviour
         {
             mainCamera.transform.position = new Vector3(transform.position.x + offset.x, transform.position.y + offset.y, offset.z);
         }
-        Animator.SetBool("IsMoving", false);
+        Animator.SetBool("isMoving", moveOn);
+       
     }
     // updates only when needed
     private void FixedUpdate()
     {
         move();
-        Animator.SetBool("isMoving", true);
+        
     }
     // proccessing inputs works with wads and directional keys
     void inputProccess()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
-        
-        moveDirection = new Vector2(moveX, moveY).normalized;
        
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        
     }
     // moves sprite according to inputs
     void move()
@@ -46,6 +52,7 @@ public class TopdownCharacterController : MonoBehaviour
       
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
        
+        move
         
     }
 
