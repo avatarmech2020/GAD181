e8561fd2a1d6ec89fb46478cea8d4b71fa76b82b
mike <1022986@student.sae.edu.au>
2021-07-28 20:03:21 +0800

    balancing the first level

--- Assets/Scripts/MoveStraight.cs
+++ Assets/Scripts/MoveStraight.cs
@@ -7,17 +7,32 @@ public class MoveStraight : MonoBehaviour
 
 
     public float speedOfObject = 10;
-
+    public bool isStarting;
+    private int i;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isStarting = true;
+        i = 50;
     }
 
     // Update is called once per frame
     void Update()
     {
         //if(isPlaying == true)
+        if (isStarting)
+        {
+            gameObject.transform.position += gameObject.transform.right * 3 * Time.deltaTime;
+            if(i >= 0)
+            {
+                i--;
+            }
+            if(i <= 0)
+            {
+                isStarting = false;
+            }
+        }
+        else
         gameObject.transform.position += gameObject.transform.right * speedOfObject * Time.deltaTime;
     }
 
