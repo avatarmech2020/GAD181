be176377c3a57c0e050277678843c37c2eca90ea
Caitie <caitlyn.s.lichnovsky@outlook.com>
2021-07-29 09:11:34 +0800

    wip treasure anim

--- Assets/Scripts/TopdownCharacterController.cs
+++ Assets/Scripts/TopdownCharacterController.cs
@@ -11,49 +11,62 @@ public class TopdownCharacterController : MonoBehaviour
     private Vector2 moveDirection;
     public Camera mainCamera;
     public Vector3 offset;
-    public Animator Animator;
-    public bool moveOn = false;
+    public bool isMoving;
+    public Animator Animator_m;
+    bool inputProcces_b = false;
     private void Start()
     {
-        Animator = GetComponent<Animator>();
-      
-            
+        Animator_m.gameObject.GetComponent<Animator>();
+
+
+
     }
     // Update is called once per frame
     void Update()
     {
         inputProccess();
-       // camera follow
+        // camera follow
         if (mainCamera)
         {
             mainCamera.transform.position = new Vector3(transform.position.x + offset.x, transform.position.y + offset.y, offset.z);
         }
-        Animator.SetBool("isMoving", moveOn);
-       
+
+
+
+        if (inputProcces_b == false)
+        {
+            Animator_m.SetBool("IsMoving", false);
+        }
+        if (inputProcces_b == true)
+        {
+            Animator_m.SetBool("IsMoving", true);
+        }
     }
     // updates only when needed
     private void FixedUpdate()
     {
         move();
-        
+        inputProcces_b = false;
+
+
     }
     // proccessing inputs works with wads and directional keys
     void inputProccess()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
-       
+
         moveDirection = new Vector2(moveX, moveY).normalized;
-        
+        inputProcces_b = true;
     }
     // moves sprite according to inputs
     void move()
     {
-      
+
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
-       
-        move
-        
+
+
+
     }
 
 
