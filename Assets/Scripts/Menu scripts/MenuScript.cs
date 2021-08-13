68fc4a3dd6dd614df9515127ac02c4cba1be8c7d
michael <1022986@student.sae.edu.au>
2021-08-13 16:07:18 +0800

    menu basically done

--- Assets/Scripts/Menu scripts/MenuScript.cs
+++ Assets/Scripts/Menu scripts/MenuScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
     public void PlayApp()
     {
+        Debug.Log("lolplay");
         // turn on the scene changer script
     }
     public void ExitApp()
@@ -13,6 +15,18 @@ public class MenuScript : MonoBehaviour
         Debug.Log("i quit");
 
         Application.Quit();
+    }
+
+    public void GoToOptions()
+    {
+        Debug.Log("options");
+        SceneManager.LoadScene("Options");
+    }
+
+    public void GoToCredits()
+    {
+        Debug.Log("credits");
+        SceneManager.LoadScene("Credits");
     }
 
 }
