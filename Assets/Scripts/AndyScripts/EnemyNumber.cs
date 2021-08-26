9beb161aaec0ed82bdfc1f17f153b1aa1b3a7e1f
Andy <axylumlol@gmail.com>
2021-08-26 12:08:35 +0800

    saving changes on enemy number script

--- Assets/Scripts/AndyScripts/EnemyNumber.cs
+++ Assets/Scripts/AndyScripts/EnemyNumber.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 public class EnemyNumber : MonoBehaviour
 {
     public int number;
+    public int arbitrary;
     public string text;
     public Text UItext;
+    public PlayerNumber pNumber;
+    public bool repeat = true;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +22,23 @@ public class EnemyNumber : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && repeat == false)
         {
-              Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-               RaycastHit hit;
-               // Casts the ray and get the first game object hit
-               Physics.Raycast(ray, out hit);
-               Debug.Log("This hit at " + hit.point );
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            // Casts the ray and get the first game object hit
+            Physics.Raycast(ray, out hit);
+            //Debug.Log("This hit at " + hit.point );
+            OnMouseDown();
+            repeat = false;
         }
+    }
+
+    void OnMouseDown()
+    {
+        arbitrary = number + pNumber.number;    //attempt at changing PlayerNumber's value through left clicking
+        pNumber.text = arbitrary.ToString();
+        Debug.Log("Mouse is down");
     }
 
     private string enemyNumber(int Enumber)
